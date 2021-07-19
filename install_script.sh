bc3576404aeb555003d7acb8dcff7c14ab7b125f
seongbae <seongbae@vivaldi.net>
2021-07-19 12:04:45 +0200

    added install script

---
+++ install_script.sh
@@ -0,0 +1,27 @@
+install_func() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "################## Package "$1" already installed"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "Installing package "  $1
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+list=(
+git
+base-devel
+)
+
+echo "Installing paru"
+cd ~
+git clone https://aur.archlinux.org/paru.git 
+cd paru
+makepkg -si
+
+tput sgr0
