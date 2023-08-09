3d23a1882985968a228399e10f1bb9ce34fbcf63
seongbae <seongbae@vivaldi.net>
2023-08-09 17:38:57 +0200

    added wezterm and hypr configs

---
+++ configs/hypr/xsetting.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-A-1 --primary
---
+++ configs/wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = {}
+
+config.color_scheme = 'Catppuccin Mocha'
+config.font_size = 14.0
+
+-- Spawn a nu shell in login mode
+config.default_prog = { '/usr/local/bin/nu', '-l' }
+
+config.disable_default_key_bindings = true
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'C',
+    mods = 'CTRL|SHIFT',
+    action = act.CopyTo 'Clipboard',
+  },
+  {
+    key = 'V',
+    mods = 'CTRL|SHIFT',
+    action = act.PasteFrom 'Clipboard',
+  },
+  {
+    key = 't',
+    mods = 'ALT',
+    action = act.SpawnTab 'DefaultDomain',
+  },
+  {
+    key = 'c',
+    mods = 'ALT',
+    action = act.CloseCurrentTab {confirm = true},
+  },
+}
+
+return config
