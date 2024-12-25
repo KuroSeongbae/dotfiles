8ff3efae9d4527560e8e1e6da5208efecda0a255
seongbae <seongbae@vivaldi.net>
2024-12-25 11:59:03 +0100

    added portage config

--- .wayinit.sh
+++ .wayinit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # to launch a different desktop then the default: desktop=desktopname startx
-desktop=${desktop:-Hyprland}
+desktop=${desktop:-Hyprland} # Hyprland sway
 dbus-run-session $desktop
